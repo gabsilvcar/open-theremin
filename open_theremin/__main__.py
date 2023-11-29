011c2984898a5d54ce70a9cbc0279b0c6aa15703
Gabriel da Silva Cardoso <cardoso.gabriel@proton.me>
2023-11-29 11:06:19 -0300

    wip

--- open_theremin/__main__.py
+++ open_theremin/__main__.py
@@ -1,8 +1,13 @@
-# from hand_detection import Detector, Webcam
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from hand_detection import Detector, Webcam
 from hand_detection_classic import DetectorClassic, WebcamClassic
 
-# detector = Detector()
-# webcam = Webcam(detector)
-
-detector = DetectorClassic()
-webcam = WebcamClassic(detector)
+app = QApplication(sys.argv)
+detector = Detector()
+webcam = Webcam(detector)
+sys.exit(app.exec())
+# detector = DetectorClassic()
+# webcam = WebcamClassic(detector)
--- open_theremin/hand_detection/webcam.py
+++ open_theremin/hand_detection/webcam.py
@@ -1,24 +1,56 @@
 import cv2
 import mediapipe as mp
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QWidget, QLabel
 from mediapipe import ImageFormat
 
+class ImageWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-class Webcam:
+        # Set window properties
+        self.setWindowTitle('Image Viewer')
+        self.setGeometry(100, 100, 800, 600)  # x, y, width, height
+
+        # Add a label and set a pixmap
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('path_to_your_image.jpg'))  # Replace with your image path
+        # Resize the label to fit the image
+        self.label.resize(self.label.pixmap().size())
+
+        # Show the window
+        self.show()
+
+class Webcam(QWidget):
     def __init__(self, detector):
+        super().__init__()
+
+        # Set window properties
+        self.setWindowTitle('Image Viewer')
+        self.setGeometry(100, 100, 800, 600)  # x, y, width, height
+        self.label = QLabel(self)
+
         # access webcam
         cap = cv2.VideoCapture(0)
 
         while True:
             # pull frame
             ret, frame = cap.read()
+            frame = cv2.flip(frame, 1)
             # mirror frame
             # frame = cv2.flip(frame, 1)
             # update landmarker results
             detector.detect_async(frame)
 
             frame = detector.draw(frame)
+
+            self.label = QLabel(self)
+            self.label.setPixmap(frame)  # Replace with your image path
+
             # display image
-            cv2.imshow("frame", frame)
+            # cv2.imshow("frame", frame)
+            self.show()
+
             if cv2.waitKey(1) == ord("q"):
                 break
 
